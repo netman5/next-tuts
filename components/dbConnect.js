4110a9d75bc8938fa064ca9e183e8447f5be2080
Ola Ishola <olaishola@hotmail.co.uk>
2022-08-05 23:04:13 +0100

    refactoring getting meetups into a reusable function

--- components/dbConnect.js
+++ components/dbConnect.js
@@ -4,4 +4,14 @@ async function connectToDB() {
   return await MongoClient.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@api-db.ofcj2.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
 }
 
+
+async function getCollections(collectionName) {
+  // const client = await connectToDB();
+  const db = client.db();
+  const collections = db.collections(collectionName);
+  console.log(collections);
+  client.close();
+  // return collections;
+}
+
 export default connectToDB;
